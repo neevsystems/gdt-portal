93506ca2348536142e7d619c039498983d005ae2
srini raju <srinivasarajui@gmail.com>
2018-06-16 08:03:59 +0530

    sample added working partially

--- backend/middleware/passport.js
+++ backend/middleware/passport.js
@@ -1,11 +1,35 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../models').User;
+const JwtStrategy   = require('passport-jwt').Strategy;
+const ExtractJwt    = require('passport-jwt').ExtractJwt;
+const SamlStrategy  = require('passport-saml').Strategy;
+const fs            = require('fs');
+const path          = require('path');
+const User          = require('../models').User;
 
 module.exports = function(passport){
     var opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
     opts.secretOrKey = CONFIG.jwt_encryption;
+
+    passport.serializeUser(function(user, done) {
+        done(null, user);
+      });
+      
+      passport.deserializeUser(function(user, done) {
+        done(null, user);
+      });
+
+    passport.use(new SamlStrategy(
+        {
+          path: '/login/callback',
+          entryPoint: CONFIG.entryPoint,
+          issuer: CONFIG.issuer,
+          cert: fs.readFileSync(path.join(__dirname, '../../certificates/SAML.cert'), 'utf-8')
+        },
+        function(profile, done) {
+            console.log(profile);
+          return done(null, {name:profile.nameID});
+        })
+      );
 
     passport.use(new JwtStrategy(opts, async function(jwt_payload, done){
         let err, user;
