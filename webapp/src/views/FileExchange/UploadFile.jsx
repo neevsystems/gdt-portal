b47a20c57d8d24ce191aefc98e00268bba077c69
srini raju <srinivasarajui@gmail.com>
2018-06-16 18:30:36 +0530

    File upload Fixed

--- webapp/src/views/FileExchange/UploadFile.jsx
+++ webapp/src/views/FileExchange/UploadFile.jsx
@@ -1,79 +1,68 @@
 import React from "react";
-import { Grid,Table, TableHead, TableRow,TableCell, TableBody ,
-    TableFooter,TablePagination,IconButton,InputLabel,Checkbox,FormControlLabel} from "material-ui";
-  import {  RegularCard, ItemGrid,CustomInput,Button} from "components";
-  import {  Edit,Delete, CloudUpload,AttachFile} from "@material-ui/icons";
-  
- class UploadFiles extends React.Component {
-    constructor(props){
-      super(props);
-      this.state={value:0,filename:''
+import {
+  Grid, Table, TableHead, TableRow, TableCell, TableBody,
+  TableFooter, TablePagination, IconButton, InputLabel, Checkbox, FormControlLabel
+} from "material-ui";
+import { RegularCard, ItemGrid, CustomInput, Button } from "components";
+import { Edit, Delete, CloudUpload, AttachFile } from "@material-ui/icons";
+
+class UploadFiles extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0, filename: ''
     };
-    this.handleChange=this.handleChange.bind(this);
-    }
-     handleChange = (event, value) => {
-        this.setState({ value });
-       this.setState({filename:this.myInput.value});
-      };
-    
-      handleChangeIndex = index => {
-        this.setState({ value: index });
-      }; 
-    render() {
-        return (<div>
-            <Grid container>
-      <ItemGrid xs={12} sm={12} md={12}>
-        
-         <RegularCard
-          cardTitle="Upload File"
-          content={
-            <div>
-                <Grid container>                
-                <ItemGrid xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="Name"
-                    id="name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </ItemGrid>
-              </Grid>
-             
-              <Grid container>
-                <ItemGrid xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="Description"
-                    id="desc"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </ItemGrid>
-              </Grid>
-              <Grid container>
-                <ItemGrid xs={12} sm={12} md={6}>
-                <input type='text' style={{'width':'50%'}} value={this.state.filename} disabled={true} />
-                <IconButton  onClick={(e) => this.myInput.click() }>
-              <AttachFile  />
-              <input id="myInput" type="file" ref={(ref) => this.myInput = ref} style={{ display: 'none' }} onChange={this.handleChange} />            
-              </IconButton>
-             
-                </ItemGrid>
-               
-                   
-              </Grid>
-            </div>
-          }
-          footer={<div><Button style={{'background-color':'#333333'}} variant="contained" color="primary" >Save</Button>
-          <Button style={{'background-color':'#333333'}} variant="contained" color="primary" >Cancel</Button></div>}
-          
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange = (event, value) => {
+    this.setState({ value });
+    this.setState({ filename: this.myInput.value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
+  render() {
+    return (<div>
+      <Grid container>
+        <ItemGrid xs={12} sm={12} md={12}>
+
+          <RegularCard
+            cardTitle="Upload File"
+            content={
+              <div>
+                <Grid container>
+                  <ItemGrid xs={12} sm={12} md={6}>
+                    <input type='text' style={{ 'width': '50%' }} value={this.state.filename} disabled={true} />
+                    <IconButton onClick={(e) => this.myInput.click()}>
+                      <AttachFile />
+                      <input id="myInput" type="file" ref={(ref) => this.myInput = ref} style={{ display: 'none' }} onChange={this.handleChange} />
+                    </IconButton>
+                  </ItemGrid>
+                </Grid>
+                <Grid container>
+                  <ItemGrid xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Description"
+                      id="desc"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </ItemGrid>
+                </Grid>
+                
+              </div>
+            }
+            footer={<div><Button style={{ 'background-color': '#333333' }} variant="contained" color="primary" >Save</Button>
+              <Button style={{ 'background-color': '#333333' }} variant="contained" color="primary" >Cancel</Button></div>}
+
           />
-           
-            </ItemGrid>
+
+        </ItemGrid>
       </Grid>
 
-        </div>);
-    }
+    </div>);
+  }
 }
 export default UploadFiles;
